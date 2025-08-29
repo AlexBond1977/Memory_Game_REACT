7e2e728798c600b666d5258a340376cd8943b0bf
Alexey Bondarev <6933666@mail.ru>
2025-08-29 18:30:46 +0300

    Реализован функционал игры

--- src/App.js
+++ src/App.js
@@ -24,12 +24,70 @@ class App extends React.Component {
             //перемешиваем карточки в массивах
             .sort(() => Math.random() - 0.5)
             // перезадаем id, чтобы не было дублирования одного id у двух одинаковых карточек
-            .map(item => ({...item, id: id++}));
+            // добавляем флаг isOpened для передачи состояния в дочерний компонент и флаг
+            // выполненного задания - нахождения пары одинаковых карточек isCompleted
+            .map(item => ({...item, id: id++, isOpened: false, isCompleted: false}));
     };
 
     // создаем функцию для принятия карточки, на которую нажали
-    choiceCardHandler(item){
-        console.log(item.name);
+    choiceCardHandler(openedItem) {
+        // при нажатии на карточку при двух открытых следующая открываться не будет
+        if (openedItem.isCompleted || this.state.cards.filter(item => item.isOpened).length >= 2) {
+            return;
+        }
+
+        // изменение состояния при клике
+        this.setState({
+            cards: this.state.cards.map(item => {
+                return item.id === openedItem.id ? {...item, isOpened: true} : item;
+            })
+        }, () => {
+            this.processChoosingCards();
+        });
+
+        // создаем подсчет количества кликов
+        // this.setState({clicks: this.state.clicks + 1});
+        this.setState({clicks: this.state.clicks + 1});
+    }
+
+    // метод обработки выбранных карточек
+    processChoosingCards() {
+        const openedCards = this.state.cards.filter(item => item.isOpened);
+        if(openedCards.length === 2) {
+            // если карточки одинаковые, они считаются открытыми в игре
+            if(openedCards[0].name === openedCards[1].name ) {
+                this.setState({
+                    cards: this.state.cards.map(item => {
+                        if(item.id === openedCards[0].id || item.id === openedCards[1].id) {
+                            item.isCompleted = true;
+                        }
+
+                        item.isOpened = false;
+                        return item;
+                    // вызываем функцию проверки открытия всех карточек
+                    }, () => {
+                        this.checkForAllCompleted();
+                    })
+                })
+            // если карточки не одинаковые, то закрываем их
+            } else {
+                setTimeout(() => {
+                    this.setState({
+                        cards: this.state.cards.map(item => {
+                            item.isOpened = false;
+                            return item;
+                        })
+                    })
+                }, 1000)
+            }
+        }
+    }
+
+    // метод проверки завершения игры
+    checkForAllCompleted(){
+        if(this.state.cards.every(item => item.isCompleted)){
+
+        }
     }
 
     render() {
@@ -43,8 +101,10 @@ class App extends React.Component {
                     <div className="cards">
                         {
                             this.state.cards.map(item => (
-                                //добавляем для передачи в дочерний компонент метода
-                                <Card item={item} key={item.id} onChoice={this.choiceCardHandler}/>
+                                //добавляем для передачи в дочерний компонент метода и позже флага состояния
+                                // и флага открытия
+                                <Card item={item} key={item.id} isShowed={item.isOpened || item.isCompleted}
+                                      onChoice={this.choiceCardHandler.bind(this)}/>
                                 //верстка переносится в компонент Card
                                 // <div className="card" key={item.id}>
                                 //     <img src={'/images/' + item.image} alt={item.name}/>
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,15 +4,18 @@ import "./Card.css"
 class Card extends React.Component {
     // создаем конструктор для использования state и отслеживания состояния открытой
     // или закрытой карточки - по умолчанию закрытая
-    constructor() {
-        super();
-        this.state = {isOpened: false};
-    }
+    // переносим в родительский компонент при реализации функционала сверки совпадений карточек и
+    // удаляем конструктор
+    // constructor() {
+    //     super();
+    //     this.state = {isOpened: false};
+    // }
 
     //создаем метод для обработки клика по карточке с вызовом метода из родительского компонента,
     //также меняем состояние карточки isOpened
     cardClickHandler(item) {
-        this.setState({isOpened: !this.state.isOpened});
+        // позже добавляем использование props родительского компонента и везде ниже
+        // this.setState({isOpened: !this.state.isOpened});
         this.props.onChoice(item);
     }
 
@@ -21,7 +24,7 @@ class Card extends React.Component {
             // переносим код из App.js с изменениями, добавляем обработчик события по клику,
             // сохранеям контекст через bind и передаем вторым параметром данные карточки
             // this.props.item
-            <div className={'card ' + (this.state.isOpened ? 'opened' : 'closed')}
+            <div className={'card ' + (this.props.isShowed ? 'opened' : 'closed')}
                  onClick={this.cardClickHandler.bind(this, this.props.item)}>
                 <div className="card-inner card-front">
                     <img src={'/images/' + this.props.item.image} alt={this.props.item.name}/>
